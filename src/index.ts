92102eb6a478c31e844af080c10bf37f305d185e
Tyler Long <tyler4long@gmail.com>
2021-09-29 11:34:26 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+(async () => {
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+  await rc.restapi().glip().groups('111');
+})();
